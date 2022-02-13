e2a26572fced76f70a95314b78252c8abcd9e423
Ickshonpe <david.curthoys@googlemail.com>
2022-02-13 11:29:06 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Bevy Heterogenous Texture Atlas Loader
+
+Load heterogenous texture atlases according to a RON file manifest.  
+  
+  
+
+# Example
+
+## Problem
+You have a beautiful sprite sheet `example.png` for your game
+
+ ![/assets/example.png](/assets/example.png)
+
+and you want to load it into Bevy but the sprites are all different sizes.  
+  
+
+
+## Solution
+
+### You create an `examples.ron` manifest file
+
+```
+(
+    path: "example.png",
+    rects: [
+        ("yellow", 16, 64, 19, 67),
+        ("face", 93, 125, 108, 139),
+        ("patches", 176, 196, 34, 68),
+    ]
+)
+```
+notes
+* The `path` is relative to the root assets directory, not to the manifest file.
+* The `rects` coords are in order min_x, max_x, min_y, max_y.
+* `rects` is a list not a map to preserve ordering. For this example, in the output texture atlas "yellow" will be texture index 0,
+    "face" will be texture index 1, and "patches" will be texture index 2, the same order as in the manifest file.  
+
+  
+
+
+### Add to your Cargo.toml `[dependencies]`
+
+```
+bevy_heterogeneous_texture_atlas_loader = { github = "https://github.com/ickshonpe/bevy_heterogeneous_texture_atlas_loader" }
+```
+### Add the Plugin to your Bevy App
+```rust
+use heterogeneous_texture_atlas_loader::*;
+pub main() {
+    let app = App::new();
+
+    ...
+
+    .add_plugin(HeterogeneousTextureAtlasLoaderPlugin);
+}
+```
+
+### Load the manifest using the AssetServer in a startup system
+```rust
+pub fn setup(
+    asset_server: Res<AssetServer>,
+) {
+ let _manifest: Handle<HeterogeneousTextureAtlasManifest> = asset_server.load("manifest.ron");
+}
+```
+
+### Plugin sends an event once the TextureAtlas is ready
+```rust
+pub fn once_atlas_loaded(
+    mut commands: Commands,
+    mut events: EventReader<HeterogeneousTextureAtlasLoadedEvent>,
+    mut manifests: Res<Assets<HeterogeneousTextureAtlasManifest>>,
+) {
+    for event in events {
+        let manifest = manifests.get(&event.0).unwrap();
+        commands
+        .spawn_bundle(SpriteSheetBundle {
+            sprite: TextureAtlasSprite::new(manifest.get("face")),
+            texture_atlas: manifest.atlas.clone(),
+            ..Default::default()
+        });
+    }
+}
+```
+#
+### FAQ
+Can I use this with bevy_asset_loader?
+> I wish
+
+Why are the names of everything so long?  
+> lols
+
+Crash during loading?
+> There might be a scheduling bug. Not fixed
+
+Example has bugs?
+> Not tested
+
+
---
+++ src/lib.rs
@@ -0,0 +1,132 @@
+use bevy::prelude::*;
+use bevy::math::vec2;
+use bevy::reflect::TypeUuid;
+use bevy::utils::HashMap;
+use bevy::asset::{LoadContext, BoxedFuture, LoadedAsset, AssetLoader};
+use bevy::sprite::TextureAtlas;
+use serde::Deserialize;
+
+#[derive(Debug, TypeUuid)]
+#[uuid = "b00584ad-0507-44ed-a89c-e6758f3576f6"]
+pub struct HeterogeneousTextureAtlasManifest {
+    path: String,
+    pub atlas: Handle<TextureAtlas>,
+    sprite_rects: Vec<(String, bevy::sprite::Rect)>,
+    pub indices: HashMap<String, usize>
+}   
+
+impl HeterogeneousTextureAtlasManifest {
+    pub fn get(&self, sprite_name: &str) -> usize {
+        self.indices[sprite_name]
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SpriteSheetManifest {
+    pub path: String,
+    pub rects: Vec<(String, u32, u32, u32, u32)>,
+}
+
+#[derive(Default)]
+pub struct HeterogeneousTextureAtlasManifestLoader;
+
+impl AssetLoader for HeterogeneousTextureAtlasManifestLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<(), anyhow::Error>> {
+        Box::pin(async move {
+            let SpriteSheetManifest { path, rects } = ron::de::from_bytes(bytes)?;
+            let sprite_rects: Vec<(String, bevy::sprite::Rect)> = rects.into_iter()
+                .map(|(name, left, right, bottom, top)| (
+                    name,
+                    bevy::sprite::Rect { 
+                        min: vec2(left as f32, bottom as f32),
+                        max: vec2(right as f32, top as f32),
+                    }
+                ))
+                .collect();
+            sprite_rects.iter().for_each(|x| println!("{:?}", x));
+            let manifest = HeterogeneousTextureAtlasManifest {
+                path,
+                atlas: Handle::default(),
+                sprite_rects,
+                indices: HashMap::default()
+            };
+            load_context.set_default_asset(LoadedAsset::new(manifest));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ron"]
+    }
+}
+
+pub fn heterogeneous_atlas_manifest_events_handler(
+    mut local: Local<HashMap<Handle<Image>, Handle<HeterogeneousTextureAtlasManifest>>>,
+    mut manifest_events: EventReader<AssetEvent<HeterogeneousTextureAtlasManifest>>,
+    mut image_events: EventReader<AssetEvent<Image>>,
+    asset_server: Res<AssetServer>,
+    mut manifestos: ResMut<Assets<HeterogeneousTextureAtlasManifest>>,
+    images: Res<Assets<Image>>,
+    mut atlases: ResMut<Assets<TextureAtlas>>,
+    mut event_writer: EventWriter<HeterogeneousTextureAtlasLoadedEvent>,
+) {
+    let image_map = &mut *local;
+    for event in manifest_events.iter() {
+        match event {
+            AssetEvent::Created { handle: manifest_handle } => {
+                let manifest = manifestos.get_mut(manifest_handle).unwrap();
+                let image_handle: Handle<Image> = asset_server.load(&manifest.path);
+                image_map.insert(image_handle.clone_weak(), manifest_handle.clone_weak());
+            },
+            _ => {},
+        }
+    }
+    if image_map.is_empty() {
+        return;
+    }
+    for event in image_events.iter() {
+        match event {
+            AssetEvent::Created { handle: image_handle } => {
+                if let Some(manifest_handle) = image_map.remove(image_handle) {
+                    let manifest = manifestos.get_mut(&manifest_handle).unwrap();
+                    let image = images.get(image_handle).unwrap();
+                    let image_dimensions = Vec2::new(
+                        image.texture_descriptor.size.width as f32,
+                        image.texture_descriptor.size.height as f32
+                    );
+                    let mut atlas = TextureAtlas::new_empty(
+                        image_handle.clone(),
+                        image_dimensions
+                    );
+                    for (name, sprite_rect) in manifest.sprite_rects.iter() {
+                        let index = atlas.add_texture(*sprite_rect);
+                        manifest.indices.insert(name.clone(), index);
+                    }
+                    let atlas_handle = atlases.add(atlas);
+                    manifest.atlas = atlas_handle;
+                    event_writer.send(HeterogeneousTextureAtlasLoadedEvent(manifest_handle));
+                }
+            },
+            _ => {}
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct HeterogeneousTextureAtlasLoadedEvent(pub Handle<HeterogeneousTextureAtlasManifest>);
+
+pub struct HeterogeneousTextureAtlasLoaderPlugin;
+
+impl Plugin for HeterogeneousTextureAtlasLoaderPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .add_event::<HeterogeneousTextureAtlasLoadedEvent>()
+        .add_asset::<HeterogeneousTextureAtlasManifest>()
+        .init_asset_loader::<HeterogeneousTextureAtlasManifestLoader>()
+        .add_system(heterogeneous_atlas_manifest_events_handler);
+    }
+}
