6d7513f8b7f9390b28d11bb6faabd206d6e178bd
Ickshonpe <david.curthoys@googlemail.com>
2022-10-11 22:39:14 +0100

    replaced weird negative size hack with a mutex

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,17 @@ use bevy::sprite::TextureAtlas;
 use bevy::utils::{HashMap, HashSet};
 use serde::Deserialize;
 use std::path::Path;
+use std::sync::Mutex;
+use lazy_static::*;
+
+lazy_static! {
+    static ref LOADING: Mutex<
+        HashMap<
+            Handle<Image>, 
+            HashSet<Handle<TextureAtlas>>
+        >
+    > = Mutex::new(HashMap::default());
+}
 
 #[derive(Debug, Deserialize)]
 struct Sprite {
@@ -53,7 +64,7 @@ impl AssetLoader for TextureAtlasLoader {
             let image_handle: Handle<Image> = load_context.get_handle(image_asset_path.clone());
 
             // create the texture atlas
-            let mut texture_atlas = TextureAtlas::new_empty(image_handle, -Vec2::ONE);
+            let mut texture_atlas = TextureAtlas::new_empty(image_handle.clone(), -Vec2::ONE);
 
             for (name, sprite_rect) in manifest.sprites.into_iter().map(|sprite| sprite.into()) {
                 let index = texture_atlas.add_texture(sprite_rect);
@@ -73,8 +84,19 @@ impl AssetLoader for TextureAtlasLoader {
                 }
             }
 
+            let asset_path = AssetPath::new(load_context.path().into(), None);
+            let texture_atlas_handle: Handle<TextureAtlas> = load_context.get_handle(asset_path);
+
             // create and return the asset
             let atlas_asset = LoadedAsset::new(texture_atlas).with_dependency(image_asset_path);
+            
+            
+            let mut loading = LOADING.lock().unwrap();
+            loading
+                .entry(image_handle.clone_weak())
+                .or_insert_with(HashSet::default)
+                .insert(texture_atlas_handle.clone_weak());
+            
             load_context.set_default_asset(atlas_asset);
             Ok(())
         })
@@ -85,52 +107,24 @@ impl AssetLoader for TextureAtlasLoader {
     }
 }
 
-#[derive(Default, Deref, DerefMut)]
-struct UnsizedAtlases(HashMap<Handle<Image>, HashSet<Handle<TextureAtlas>>>);
-
 fn set_texture_atlas_size(
-    mut unsized_atlases: Local<UnsizedAtlases>,
-    mut atlas_asset_events: EventReader<AssetEvent<TextureAtlas>>,
     mut image_asset_events: EventReader<AssetEvent<Image>>,
     images: Res<Assets<Image>>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    for atlas_asset_event in atlas_asset_events.iter() {
-        if let AssetEvent::Created {
-            handle: texture_atlas_handle,
-        } = atlas_asset_event
-        {
-            if let Some(texture_atlas) = texture_atlases.get(texture_atlas_handle) {
-                if texture_atlas.size.x < 0. {
-                    if let Some(image) = images.get(&texture_atlas.texture) {
-                        let texture_atlas = texture_atlases.get_mut(texture_atlas_handle).unwrap();
-                        texture_atlas.size = image.size();
-                    } else {
-                        unsized_atlases
-                            .entry(texture_atlas.texture.clone_weak())
-                            .or_insert_with(HashSet::default)
-                            .insert(texture_atlas_handle.clone_weak());
-                    }
-                }
-            }
-        }
-    }
-
     for image_asset_event in image_asset_events.iter() {
         if let AssetEvent::Created {
             handle: image_handle,
         } = image_asset_event
         {
-            if let Some(atlases) = unsized_atlases.get_mut(image_handle) {
+            let mut loading = LOADING.lock().unwrap();
+            if let Some(atlases) = loading.get_mut(image_handle) {
                 for atlas_handle in atlases.drain() {
-                    if let Some(texture_atlas) = texture_atlases.get(&atlas_handle) {
-                        if texture_atlas.size.x < 0. {
-                            let texture_atlas = texture_atlases.get_mut(&atlas_handle).unwrap();
-                            texture_atlas.size = images.get(image_handle).unwrap().size();
-                        }
+                    if let Some(texture_atlas) = texture_atlases.get_mut(&atlas_handle) {
+                        texture_atlas.size = images.get(image_handle).unwrap().size();
                     }
                 }
-                unsized_atlases.remove(image_handle);
+                loading.remove(image_handle);
             }
         }
     }
